888b8a02faca65cc8e4302fb59b9ccb8aa522030
michalkarazniewicz80-prog <michal.karazniewicz80@gmail.com>
2025-11-27 14:46:52 +0000

    Update loadDrafts.js

--- netlify/functions/loadDrafts.js
+++ netlify/functions/loadDrafts.js
@@ -8,8 +8,8 @@ exports.handler = async (event) => {
     }
 
     onst SUPABASE_URL = process.env.SUPABASE_URL;
-    const SUPABASE_SERVICE_ROLE = process.env.SUPABASE_SERVICE_ROLE;
-    const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE);
+    const SUPABASE_SERVICE_ROLE = process.env.SUPABASE_ANON_KEY;
+    const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
     const { data, error } = await supabase
       .from('iso_drafts')
